5c5dc57e24ddabbdf44afb4ee2a7d12a2d96be52
Diana Liao <dijali@gmail.com>
2021-04-10 15:10:58 -0400

    commit tree 70f5ae0111d072fb20a4afc81b40362fbbbc3914

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(e => e.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name){
+  return drivers.filter(e => e.startsWith(name))
+}
+
+function matchName(drivers, name){
+  return drivers.filter(e => e.name === name)
+}
